da0f3b4352ca00fe6b82fe83590b6cf71b96981e
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-09-02 18:33:54 +0530

    Added utilities for package - exterior

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { ThreadHandler } from "./Handlers/ThreadHandler.js";
 export { SlashCommandHandler } from "./Handlers/SlashCommandHandler.js";
+export { GuildResolver, ChannnelResolver } from "./Utilities/Resolver_Utils.js";
