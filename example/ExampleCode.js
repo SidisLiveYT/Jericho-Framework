dc78c43b70bd77f3b7f388e0b34fb017e2853cd8
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-09-02 10:30:13 +0530

    chore(example file updated): added Slash Command Workflow

--- example/ExampleCode.js
+++ example/ExampleCode.js
@@ -1,5 +1,6 @@
 import {
-    ThreadHandler
+    ThreadHandler,
+    SlashCommandHandler
 } from '../src/index';
 
 //Creating Channel Instance for Particular Text Channel
@@ -12,6 +13,17 @@ const Thread1 = ThreadInstance1.Thread;
 
 const ThreadInstance2 = ChannelInstance.CreateThread(Options);
 const Thread2 = ThreadInstance2.Thread;
+
+//Slash Command handler Usage
+const SlashCommandInstance = new SlashCommandHandler(Client, {
+    guild: message,
+    global: false,
+});
+//Slash Command Handler to Set Commands
+SlashCommandInstance.set(Commands);
+//Slash Command Handler to Deploy Commands
+SlashCommandInstance.deploy();
+
 
 console.log(Thread1);
 console.log(Thread2);
