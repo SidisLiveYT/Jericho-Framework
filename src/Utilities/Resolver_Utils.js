4637bb93f80338f566cccf534cb764f03ccd8cec
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-09-05 15:32:25 +0530

    feat(voice handler for v2): v2 last Feature Voice Handler has been Added Completely

--- src/Utilities/Resolver_Utils.js
+++ src/Utilities/Resolver_Utils.js
@@ -1,4 +1,6 @@
-import { Channel, Client, Guild, GuildChannel } from 'discord.js'
+import {
+  Channel, Client, Guild, GuildChannel,
+} from 'discord.js';
 
 /**
  * @function ChannnelResolver Custom Resolvable for Channel
@@ -10,31 +12,31 @@ import { Channel, Client, Guild, GuildChannel } from 'discord.js'
 
 export function ChannnelResolver(Client, ChannelResolve, Extraif) {
   if (Extraif.ifmessage && ChannelResolve.channel) {
-    return ChannelFilterType(ChannelResolve.channel, Extraif)
+    return ChannelFilterType(ChannelResolve.channel, Extraif);
   }
   if (ChannelResolve.id && ChannelResolve.type) {
-    return ChannelFilterType(ChannelResolve, Extraif)
+    return ChannelFilterType(ChannelResolve, Extraif);
   }
   if (
-    typeof ChannelResolve !== 'string' ||
-    typeof ChannelResolve !== 'number'
+    typeof ChannelResolve !== 'string'
+    || typeof ChannelResolve !== 'number'
   ) {
-    throw TypeError('Invalid ChannelResolve is Detected!')
+    throw TypeError('Invalid ChannelResolve is Detected!');
   } else if (!Number.isNaN(`${ChannelResolve}`)) {
     if (Client.channels.cache.get(`${ChannelResolve}`)) {
       return ChannelFilterType(
         Client.channels.cache.get(`${ChannelResolve}`),
         Extraif,
-      )
+      );
     }
     return Client.channels
       .fetch(`${ChannelResolve}`)
       .then((Channel) => ChannelFilterType(Channel, Extraif))
       .catch((error) => {
-        throw TypeError(`Invalid Channel.id : ${error}`)
-      })
+        throw TypeError(`Invalid Channel.id : ${error}`);
+      });
   }
-  return ChannelFilterType(ChannelResolve, Extraif)
+  return ChannelFilterType(ChannelResolve, Extraif);
 
   /**
    * @function ChannelFilterType Returning the Channel Type After Getting Accurate Resolve form Source
@@ -45,66 +47,66 @@ export function ChannnelResolver(Client, ChannelResolve, Extraif) {
 
   function ChannelFilterType(Channel, Extraifconditions) {
     if (
-      Channel.type === 'GUILD_PUBLIC_THREAD' ||
-      Channel.type === 'GUILD_PRIVATE_THREAD' ||
-      Channel.type === 'GUILD_TEXT' ||
-      Channel.type === 'GUILD_VOICE' ||
-      Channel.type === 'GUILD_STAGE_VOICE'
+      Channel.type === 'GUILD_PUBLIC_THREAD'
+      || Channel.type === 'GUILD_PRIVATE_THREAD'
+      || Channel.type === 'GUILD_TEXT'
+      || Channel.type === 'GUILD_VOICE'
+      || Channel.type === 'GUILD_STAGE_VOICE'
     ) {
       if (
-        Extraifconditions &&
-        Extraifconditions.type === 'publicthread' &&
-        Channel.type === 'GUILD_PUBLIC_THREAD'
+        Extraifconditions
+        && Extraifconditions.type === 'publicthread'
+        && Channel.type === 'GUILD_PUBLIC_THREAD'
       ) {
-        return Channel
+        return Channel;
       }
       if (
-        Extraifconditions &&
-        Extraifconditions.type === 'privatethread' &&
-        Channel.type === 'GUILD_PRIVATE_THREAD'
+        Extraifconditions
+        && Extraifconditions.type === 'privatethread'
+        && Channel.type === 'GUILD_PRIVATE_THREAD'
       ) {
-        return Channel
+        return Channel;
       }
       if (
-        Extraifconditions &&
-        Extraifconditions.type === 'thread' &&
-        Channel.isThread()
+        Extraifconditions
+        && Extraifconditions.type === 'thread'
+        && Channel.isThread()
       ) {
-        return Channel
+        return Channel;
       }
       if (
-        Extraifconditions &&
-        Extraifconditions.type === 'stage' &&
-        Channel.type === 'GUILD_STAGE_VOICE'
+        Extraifconditions
+        && Extraifconditions.type === 'stage'
+        && Channel.type === 'GUILD_STAGE_VOICE'
       ) {
-        return Channel
+        return Channel;
       }
       if (
-        Extraifconditions &&
-        Extraifconditions.type === 'voice' &&
-        Channel.isVoice()
+        Extraifconditions
+        && Extraifconditions.type === 'voice'
+        && Channel.isVoice()
       ) {
-        return Channel
+        return Channel;
       }
       if (
-        Extraifconditions &&
-        Extraifconditions.type === 'allvoice' &&
-        (Channel.isVoice() || Channel.type === 'GUILD_STAGE_VOICE')
+        Extraifconditions
+        && Extraifconditions.type === 'allvoice'
+        && (Channel.isVoice() || Channel.type === 'GUILD_STAGE_VOICE')
       ) {
-        return Channel
+        return Channel;
       }
       if (
-        Extraifconditions &&
-        Extraifconditions.type === 'text' &&
-        Channel.type === 'GUILD_TEXT'
+        Extraifconditions
+        && Extraifconditions.type === 'text'
+        && Channel.type === 'GUILD_TEXT'
       ) {
-        return Channel
+        return Channel;
       }
-      return Channel
+      return Channel;
     }
     throw SyntaxError(
       '[Wrong Channel Resolve] : Provide Channel.id or Channel Collection of discord.js v13',
-    )
+    );
   }
 }
 
@@ -117,24 +119,24 @@ export function ChannnelResolver(Client, ChannelResolve, Extraif) {
  */
 
 export function GuildResolver(Client, GuildResolve, Extraif) {
-  if (Extraif.ifmessage && GuildResolve.guild) return GuildResolve.guild
+  if (Extraif.ifmessage && GuildResolve.guild) return GuildResolve.guild;
   if (GuildResolve.id && GuildResolve.members && GuildResolve.channels) {
-    return GuildResolve
+    return GuildResolve;
   }
   if (typeof GuildResolve !== 'string' || typeof GuildResolve !== 'number') {
-    throw TypeError('Invalid GuildResolve is Detected!')
+    throw TypeError('Invalid GuildResolve is Detected!');
   } else if (!Number.isNaN(`${GuildResolve}`)) {
     if (Client.guilds.cache.get(`${GuildResolve}`)) {
-      return Client.guilds.cache.get(`${GuildResolve}`)
+      return Client.guilds.cache.get(`${GuildResolve}`);
     }
     return Client.guilds
       .fetch(`${GuildResolve}`)
       .then((Guild) => Guild)
       .catch((error) => {
-        throw TypeError(`Invalid Guild.id : ${error}`)
-      })
+        throw TypeError(`Invalid Guild.id : ${error}`);
+      });
   }
-  return GuildResolve
+  return GuildResolve;
 }
 
 /**
@@ -145,9 +147,9 @@ export function GuildResolver(Client, GuildResolve, Extraif) {
  */
 
 export function BooleanResolver(FirstHand, SecondHand) {
-  if (![true, false].includes(FirstHand)) return SecondHand
-  if (![true, false].includes(SecondHand)) return FirstHand
-  if (FirstHand) return FirstHand
-  if (SecondHand) return SecondHand
-  return false
+  if (![true, false].includes(FirstHand)) return SecondHand;
+  if (![true, false].includes(SecondHand)) return FirstHand;
+  if (FirstHand) return FirstHand;
+  if (SecondHand) return SecondHand;
+  return false;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { ThreadHandler } from './Handlers/ThreadHandler.js'
-export { SlashCommandHandler } from './Handlers/SlashCommandHandler.js'
-export { GuildResolver, ChannnelResolver } from './Utilities/Resolver_Utils.js'
-export { VoiceHandler } from './Handlers/VoiceHandler.js'
+export { ThreadHandler } from './Handlers/ThreadHandler.js';
+export { SlashCommandHandler } from './Handlers/SlashCommandHandler.js';
+export { GuildResolver, ChannnelResolver } from './Utilities/Resolver_Utils.js';
+export { VoiceHandler } from './Handlers/VoiceHandler.js';
