0c984c162a4821e8c5ac066bdd7875268dcebf99
Sid is Live YT <sidisliveytofficial@gmail.com>
2021-09-06 12:02:17 +0530

    fix(fixed typings): fixed Typings and Declaration of the package as a module

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+export { ThreadHandler } from '../src/Handlers/ThreadHandler'
+export { SlashCommandHandler } from '../src/Handlers/SlashCommandHandler'
+export { VoiceHandler } from '../src/Handlers/VoiceHandler'
+declare module 'jericho-framework'
